d2ad8e6ad0f38260d5a7e176ca631748e7db068b
klimisnotavailable <klimgrigor28@gmail.com>
2024-04-26 22:30:44 +0300

    update main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   const [filmsData, setFilmsData] = useState[];
+  
+  
+    <>
+    </>
+  )
+}
+
+export default App
